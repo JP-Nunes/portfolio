90a4231470ab4deaa82117552c68f22d2573f42a
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-02 00:02:05 -0300

    Adding template engine nunjucks

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+server.set('view engine', 'html')
+
+nunjucks.configure('views', {
+    express:server
+})
+
 server.get('/', function(req, res) {
-    return res.send("Hello!")
+    return res.render('about.html')
+})
+server.get('/portfolio', function(req, res) {
+    return res.render('portfolio.html')
 })
 
 server.listen(5000, function() {
-    console.log("server is running")
+    console.log('server is running')
 })
