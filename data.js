94e6662ff5a911c33e169406374d94daa1da83b2
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-02 18:09:19 -0300

    Using nunjucks to make html pages more dynamic

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "2DJtGCesOr4",
+        title: "Envio de e-mails em massa  com Amazon SES, Node.JS, Typescript e Redis",
+        duration: "57 min",
+        price: "free"
+    },
+    {
+        id: "P65RJTTqkN4",
+        title: "Formulários no ReactJS e React Native com Unform",
+        duration: "49 min",
+        price: "free"
+    },
+    {
+        id: "C8TrGTgvLkc",
+        title: "App (RS/XP)Cache de imagens e regulamento",
+        duration: "37 min",
+        price: "free"
+    },
+    {
+        id: "3t78tTWt-JY",
+        title: "App (RS/XP) Iniciando projeto com Adonis",
+        duration: "59 min",
+        price: "free"
+    },
+    {
+        id: "tjgCJgm8K3A",
+        title: "Nova API do React Navigation",
+        duration: "62 min",
+        price: "free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática",
+        duration: "45 min",
+        price: "free"
+    }
+]
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const videos = require('./data')
 
 const server = express()
 
 server.use(express.static('public'))
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
     express:server
 })
 
 server.get('/', function(req, res) {
-    return res.render('about.html')
+    return res.render('about')
 })
 server.get('/portfolio', function(req, res) {
-    return res.render('portfolio.html')
+    return res.render('portfolio', { items: videos })
 })
 
 server.listen(5000, function() {
